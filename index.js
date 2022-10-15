3cb2a8571dc6339209c970122be175c936db14b6
LekhaKumar <lekhahamsa613@gmail.com>
2022-10-15 20:44:01 +0800

    Successfully added js for rating

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 
+
+  $(".rate").click(function (e) {
+    e.preventDefault();
+    let clickingEvent = $(this).attr("id");
+    switch (clickingEvent) {
+      case "one":
+        $("span").text("1");
+        break;
+      case "two":
+        $("span").text("2");
+        break;
+      case "three":
+        $("span").text("3");
+        break;
+      case "four":
+        $("span").text("4");
+        break;
+      case "five":
+        $("span").text("5");
+        break;
+      default:
+       $(".selection").text("You have selected none of them.");
+        break;
+    }
+  
+  
+  });
+  
+
+
 $(".submit_btn").click(function () {
+
 
   $("section").removeClass("hide_card");
 
   $("#rating_card").addClass("hide_card");
 
-  $(".rate").click(function () {
-    let chosenRating = $(this).attr("value");
-    
-      switch (chosenRating) {
-        case "1":
-          $("span").text("1");
-    
-          break;
-        case "2":
-          $("span").text("2");
-    
-          break;
-        case "3":
-          $("span").text("3");
-    
-          break;
-        case "4":
-          $("span").text("4");
-    
-          break;
-        case "5":
-          $("span").text("5");
-    
-          break;
-    
-        default:
-          break;
-      }
-    
-    }
-    
-  );
 
 
- 
-  
 
 });
 
