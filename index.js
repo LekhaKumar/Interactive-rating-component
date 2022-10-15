b4956c05bf8e2f5caeabd6719551bd1bf8593a54
LekhaKumar <lekhahamsa613@gmail.com>
2022-10-15 18:41:05 +0800

    JQuery codes

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+ 
+$(".submit_btn").click(function () { 
+
+  let click = $(this).attr("id");
+  
+  clickRating(click);
+  $("section").removeClass("hide_card");
+  $("#rating_card").addClass("hide_card");
+  
+});
+
+
+function clickRating(key) {
+
+switch (key) {
+
+  case "one":
+    $(key).click(function () { 
+      $(".selection").text("You selected 1 out of 5");
+      
+    });
+   
+    break;
+  case "two":
+    $(".selection").text("You selected 2 out of 5");
+    break;
+  case "three":
+    $(".selection").text("You selected 3 out of 5");
+    break;
+  case "four":
+    $(".selection").text("You selected 4 out of 5");
+    break;
+  case "five":
+    $(".selection").text("You selected 5 out of 5");
+    break;
+  default:
+    $(".selection").text("You selected none.");
+    break;
+}
+
+  
+}
 
