3d7ad492ff3dc1f5f338008404a2d0455a99cd90
LekhaKumar <lekhahamsa613@gmail.com>
2022-10-20 11:45:58 +0800

    changed the structure of clicking event

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 
   $(".rate").click(function (e) {
     e.preventDefault();
-    let clickingEvent = $(this).attr("id");
-    switch (clickingEvent) {
-      case "one":
-        $("span").text("1");
-        break;
-      case "two":
-        $("span").text("2");
-        break;
-      case "three":
-        $("span").text("3");
-        break;
-      case "four":
-        $("span").text("4");
-        break;
-      case "five":
-        $("span").text("5");
-        break;
-      default:
-       $(".selection").text("You have selected none of them.");
-        break;
-    }
-
-  
+    let value = $(this).attr("value");
+    $("span").text(value);
   });
 
 $(".submit_btn").click(function (e) {
